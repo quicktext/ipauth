97f20d0df8dc7a5d6c625c1cecb6e231bcd91093
root <root@localhost.localdomain>
2019-09-24 23:26:37 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ipauth
-This is an ipauth system, it's used in some modules of the Quicktext infomation system
+This is an ipauth system, it's used in some modules of the Quicktext information system.
+
+It's based on the IPUtil.
+
+IPUtil: <https://github.com/leoner/iputil>
---
+++ server.js
@@ -0,0 +1,32 @@
+
+var http = require('http');
+const url = require('url');
+var IpUtil = require('./iputil');
+var ipUtil = new IpUtil('./auth.txt', 'utf8');
+
+function getClientIP(req) {
+    return req.headers['x-forwarded-for'] || 
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress || 
+        req.connection.socket.remoteAddress;
+};
+
+var server = http.createServer(function(req, res) {
+  if(req.url != '/favicon.ico'){
+      var client_ip = getClientIP(req);
+      console.log("Client IP: " + client_ip);
+      var ip = ipUtil.getIpInfo(client_ip);
+      if(ip !=null){
+	result = ip.address; 
+      	res.writeHead(200, {'Content-Type': 'application/json'});
+      	data = {"auth_state":"true","auth_address:" : result,"client_ip": client_ip};
+      	res.end(JSON.stringify(data));
+      }else{
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        data = {"auth_state":"false","auth_address" : "invalid address","client_ip":client_ip};
+        res.end(JSON.stringify(data));
+      }
+    }
+});
+
+server.listen(4000,'0.0.0.0');
